3975e0a01e90c575c8ab4339c5f20c96b3f6651c
kj3004 <137755543+kj3004@users.noreply.github.com>
2024-12-23 18:02:07 +0530

    color changed, added video background

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,154 +19,165 @@ import Newsletter from '../shared/Newsletter';
 
 
 const Home = () => {
-  return <>
-   
-  <section>
-    <Container>
-      <Row>
-        <Col lg='6'>
-        <div className="hero__content">
-          <div className="hero__subtitle d-flec align-items-center">
-            <Subtitle subtitle={'Know Before You Go'}/>
-            <img src={worldImg} alt=""/>
-          </div>
-          <h1>Welcome to Wonder of Asia {""}
-            <span className="highlight">
-              Sri Lanka
-            </span>
-          </h1>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam possimus saepe labore inventore, quidem quasi. Iste in voluptates voluptas beatae commodi delectus sunt modi magni, error doloremque vel cumque id!
-          </p>
-        </div>
-        </Col>
-        <Col lg='2'>
-        <div className="hero__img-box">
-          <img src={heroImg} alt="" />
-        </div>
-        </Col>
-        <Col lg='2'>
-        <div className="hero__img-box hero__video-box mt-4">
-          <video src={heroVideo} alt="" controls/>
-        </div>
-        </Col>
-        <Col lg='2'>
-        <div className="hero__img-box mt-5">
-          <img src={heroImg02} alt="" />
-        </div>
-        </Col>
-        <SearchBar/>
-      </Row>
-    </Container>
-  </section>
-
-  {/* hero secion start*/}
-  <section>
-    <Container>
-      <Row>
-        <Col lg="3">
-        <h5 className="services__subtitle">What we serve</h5>
-        <h2 className="services__title">we offer our best services</h2>
-
-        </Col>
-        <ServiceList/>
-      </Row>
-    </Container>
-  </section>
-
-  {/*  featured tour collection start    */ }
-
-<section>
-  <Container>
-    <Row>
-      <Col lg="12" className="mb-5">
-      <Subtitle subtitle={"Explore"}  />
-      <h2 className="featured__tour-title">Our featured tours</h2>
-      </Col>
-      <FeaturedTourList/>
-    </Row>
-  </Container>
-</section>
-
-  {/*  featured tour collection end    */ }
-
-  {/*  experiences start    */ }
- 
- <section>
-  <Container>
-    <Row>
-      <Col lg ="6">
-      <div className="experince__content">
-        <Subtitle subtitle ={"Experience"}/>
-        <h2>
-          With our all experience <br/>we will serve You
-        </h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, repudiandae numquam! Et similique debitis cupiditate doloremque, dignissimos reprehenderit delectus adipisci, expedita dicta repellat excepturi quibusdam sint fugiat, eveniet possimus. Harum.
-        </p>
-      </div>
-      <div className="counter__wrapper d-flex align-items-center gap-5">
-        <div className="counter__box">
-          <span>12k+</span>
-          <h6>Successful Trip</h6>
-        </div>
-        <div className="counter__box">
-          <span>2k+</span>
-          <h6>Regular Clients</h6>
-        </div>
-        <div className="counter__box">
-          <span>15</span>
-          <h6>Years Experiences</h6>
-        </div>
-      </div>
-      </Col>
-      <Col lg ="6">
-       <div className="exeperience__img">
-        <img src={experienceImg} alt="" />
-       </div>
-      </Col>
-    </Row>
-  </Container>
- </section>
-
-   {/*  experiences ends    */ }
-
-     {/* gallery starts    */ }
-      <section>
+  return (
+    <>
+      <section className="hero-section">
+        <video src={heroVideo} autoPlay muted loop className="video-bg" />
+        <div className="bg-overlay"></div>
         <Container>
           <Row>
-            <Col lg ="12">
-            <Subtitle subtitle = {"Gallery"}/>
-            <h2 className="gallery__title">
-              Visit our customers tour gallery</h2>
+            <Col lg="6">
+              <div className="hero__content">
+                <div className="hero__subtitle d-flec align-items-center">
+                  <Subtitle subtitle={"Know Before You Go"} />
+                  <img src={worldImg} alt="" />
+                </div>
+                <h1 style={{ color: "#DAA520" }}>
+                  Welcome to Wonder of Asia {""}
+                  <span className="highlight">Sri Lanka</span>
+                </h1>
+                <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Laboriosam possimus saepe labore inventore, quidem quasi. Iste
+                  in voluptates voluptas beatae commodi delectus sunt modi
+                  magni, error doloremque vel cumque id!
+                </p>
+              </div>
             </Col>
-            <Col lg="12">
-            <MasonryImagesGallery/>
+            <Col lg="2">
+              <div className="hero__img-box">
+                <img src={heroImg} alt="" />
+              </div>
+            </Col>
+            <Col lg="2">
+              <div className="hero__img-box hero__video-box mt-4">
+                <video src={heroVideo} alt="" controls />
+              </div>
+            </Col>
+            <Col lg="2">
+              <div className="hero__img-box mt-5">
+                <img src={heroImg02} alt="" />
+              </div>
             </Col>
           </Row>
         </Container>
       </section>
 
-    {/* gallery ends    */ }
+      {/* hero secion start*/}
+      <section>
+        <Container>
+          <Row>
+            <Col lg="3">
+              <h5 className="services__subtitle">What we serve</h5>
+              <h2 className="services__title">we offer our best services</h2>
+            </Col>
+            <ServiceList />
+            <SearchBar />
+          </Row>
+        </Container>
+      </section>
 
-    {/* testimonial section starts  */ }
-  <section>
-    <Container>
-      <Row>
-        <Col lg = "12">
-        <Subtitle subtitle ={"Fans Love"} />
-        <h2 className="testimonial__title">What our fans says about us</h2>
-        </Col>
-        <Col lg ="12">
-        <Testimonials/>
-        </Col>
-      </Row>
-    </Container>
-  </section>
-    {/* testimonial section ends  */ }
-<Newsletter/>
+      {/*  featured tour collection start    */}
 
-  </>
+      <section>
+        <Container>
+          <Row>
+            <Col lg="12" className="mb-5">
+              <Subtitle subtitle={"Explore"} />
+              <h2 className="featured__tour-title">Our featured tours</h2>
+            </Col>
+            <FeaturedTourList />
+          </Row>
+        </Container>
+      </section>
+
+      {/*  featured tour collection end    */}
+
+      {/*  experiences start    */}
+
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6">
+              <div className="experince__content">
+                <Subtitle subtitle={"Experience"} />
+                <h2>
+                  With our all experience <br />
+                  we will serve You
+                </h2>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Neque, repudiandae numquam! Et similique debitis cupiditate
+                  doloremque, dignissimos reprehenderit delectus adipisci,
+                  expedita dicta repellat excepturi quibusdam sint fugiat,
+                  eveniet possimus. Harum.
+                </p>
+              </div>
+              <div className="counter__wrapper d-flex align-items-center gap-5">
+                <div className="counter__box">
+                  <span>12k+</span>
+                  <h6>Successful Trip</h6>
+                </div>
+                <div className="counter__box">
+                  <span>2k+</span>
+                  <h6>Regular Clients</h6>
+                </div>
+                <div className="counter__box">
+                  <span>15</span>
+                  <h6>Years Experiences</h6>
+                </div>
+              </div>
+            </Col>
+            <Col lg="6">
+              <div className="exeperience__img">
+                <img src={experienceImg} alt="" />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+      {/*  experiences ends    */}
+
+      {/* gallery starts    */}
+      <section>
+        <Container>
+          <Row>
+            <Col lg="12">
+              <Subtitle subtitle={"Gallery"} />
+              <h2 className="gallery__title">
+                Visit our customers tour gallery
+              </h2>
+            </Col>
+            <Col lg="12">
+              <MasonryImagesGallery />
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+      {/* gallery ends    */}
+
+      {/* testimonial section starts  */}
+      <section>
+        <Container>
+          <Row>
+            <Col lg="12">
+              <Subtitle subtitle={"Fans Love"} />
+              <h2 className="testimonial__title">
+                What our fans says about us
+              </h2>
+            </Col>
+            <Col lg="12">
+              <Testimonials />
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      {/* testimonial section ends  */}
+      <Newsletter />
+    </>
+  );
     
   
 }
